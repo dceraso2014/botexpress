6c2b24da0a28c5213d9c6441659ef46b06495260
Diego Ceraso <dceraso@smartqube.com.ar>
2018-06-05 16:38:15 -0300

    new bot express

--- app.js
+++ app.js
@@ -1,41 +1,64 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
 
-var restify = require('restify');
-var builder = require('botbuilder');
-//var botbuilder_azure = require("botbuilder-azure");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+// Web app
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register your web app routes here
+app.get('/', function (req, res, next) {
+  res.render('index', { title: 'Contoso Flowers' });
 });
-  
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata
+
+// Register Checkout page
+var checkout = require('./checkout');
+app.use('/checkout', checkout);
+
+// Register Bot
+var bot = require('./bot');
+app.post('/api/messages', bot.listen());
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
+// Error handlers
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
+// Development error handler, will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-//var tableName = 'botdata';
-//var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-//var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+// Production error handler, no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-//bot.set('storage', tableStorage);
-
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+// Start listening
+var port = process.env.port || process.env.PORT || 3978;
+app.listen(port, function () {
+  console.log('Web Server listening on port %s', port);
 });
